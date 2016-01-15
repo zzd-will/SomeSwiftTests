ef984cbc33db2056d4a5addddbc86c26f2d6a221
zhangzedong <zedong_zhang@hiersun.com>
2016-01-15 10:36:40 +0800

    info

--- ClientPickerTableView/BaseEX/PopAnimator.swift
+++ ClientPickerTableView/BaseEX/PopAnimator.swift
@@ -19,7 +19,9 @@ class PopAnimator: NSObject,UIViewControllerAnimatedTransitioning {
     
     //动画执行的方法
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        
+        /**
+         UIViewControllerAnimatedTransitioning的协议都包含一个对象：transitionContext，通过这个对象能获取到切换时的上下文信息，比如从哪个VC切换到哪个VC等。我们从transitionContext获取containerView，这是一个特殊的容器，切换时的动画将在这个容器中进行；UITransitionContextFromViewControllerKey和UITransitionContextToViewControllerKey就是从哪个VC切换到哪个VC，容易理解；除此之外，还有直接获取view的UITransitionContextFromViewKey和UITransitionContextToViewKey等。
+         */
         let containerView = transitionContext.containerView()
         let toView = transitionContext.viewForKey(UITransitionContextToViewKey)
         containerView!.addSubview(toView!)
